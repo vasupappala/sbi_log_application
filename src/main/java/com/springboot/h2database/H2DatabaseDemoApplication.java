88c973210c4c387635730fcc20fd2286dbdfb2b2
vasupappala <vasupappala2535@gmail.com>
2025-05-26 17:11:31 +0530

    code updated

--- src/main/java/com/springboot/h2database/H2DatabaseDemoApplication.java
+++ src/main/java/com/springboot/h2database/H2DatabaseDemoApplication.java
@@ -1,5 +1,5 @@
 package com.springboot.h2database;
-
+//Git Class
 //SG Programming channel
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
