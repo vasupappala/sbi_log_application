347caf583d4fc496c496208bd702ddd1517923a7
vasupappala <vasupappala2535@gmail.com>
2025-05-26 17:22:54 +0530

    bug fix

--- src/main/java/com/springboot/h2database/H2DatabaseDemoApplication.java
+++ src/main/java/com/springboot/h2database/H2DatabaseDemoApplication.java
@@ -1,5 +1,6 @@
 package com.springboot.h2database;
 //Git Class
+//hello
 //SG Programming channel
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
