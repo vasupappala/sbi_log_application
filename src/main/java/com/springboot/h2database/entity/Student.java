db1919874fb056a79243d3d5d7ab6e2c1daee179
vasupappala <vasupappala2535@gmail.com>
2025-05-28 06:59:28 +0530

    change for line 1 or store 101

--- src/main/java/com/springboot/h2database/entity/Student.java
+++ src/main/java/com/springboot/h2database/entity/Student.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//Git practices First
 @Entity
 @Table
 public class Student {
